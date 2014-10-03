fe8a83f8c457b0909aaa2f7a031cf2eedc05efd8
Sebastian Quintana <seba@seba.(none)>
2014-10-02 21:59:52 -0300

    first commit

---
+++ src/main/java/com/model/carta/Carta.java
@@ -0,0 +1,19 @@
+package com.model.carta;
+
+public class Carta {
+
+	private String restaurant;
+	
+	public Carta(String restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	public String getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(String restaurant) {
+		this.restaurant = restaurant;
+	}
+	
+}
